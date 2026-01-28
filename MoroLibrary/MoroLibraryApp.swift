3c833207fc9d1b4d9717fcb9b18850140c59acb9
Bruno Madeira <madeirabruno624@gmail.com>
2026-01-28 10:40:04 +0000

    Initial Commit

---
+++ MoroLibrary/MoroLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoroLibraryApp.swift
+//  MoroLibrary
+//
+//  Created by Bruno Madeira on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoroLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
