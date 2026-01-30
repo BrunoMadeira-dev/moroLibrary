0b54dea977786229903c24941a74f23c59dbdbb1
Bruno Madeira <madeirabruno624@gmail.com>
2026-01-30 08:36:05 +0000

    Added more tests to the bookListViewModel

--- MoroLibraryTests/ViewModels/BookListViewModelTests.swift
+++ MoroLibraryTests/ViewModels/BookListViewModelTests.swift
@@ -27,4 +27,35 @@ final class BookListViewModelTests: XCTestCase {
         XCTAssertEqual(vm.books.first?.title, "Book 1")
         XCTAssertEqual(vm.books.last?.title, "Book 20")
     }
+    
+    func testLoadNextPageAppendsBooks() async {
+        let mockService = MockBookServices()
+        mockService.result = .success(.mockPage1)
+        
+        let vm = BookListViewModel(service: mockService)
+        await vm.loadInitialBooks()
+        
+        XCTAssertEqual(vm.books.count, 20)
+        
+        mockService.result = .success(.mockPage2)
+        
+        await vm.loadNextPage()
+        
+        XCTAssertEqual(vm.books.count, 40)
+        XCTAssertEqual(vm.books.last?.title, "Book 40")
+    }
+    
+    func testLoadBooksFailureShowError() async {
+        let mockService = MockBookServices()
+        
+        mockService.result = .failure(NetworkError.badResponse)
+        
+        let vm = BookListViewModel(service: mockService)
+        
+        await vm.loadInitialBooks()
+        
+        XCTAssertNotNil(vm.uiError)
+        XCTAssertEqual(vm.uiError?.title, "Error")
+        XCTAssertTrue(vm.books.isEmpty)
+    }
 }
